6baec73cdd6c935aac982ae6ee50e61276e22dd6
Kosuke Nagano <gm_kou@mac.com>
2012-01-16 01:41:32 +0900

    Modify to commit string.

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'sinatra'
+require 'haml'
+require './models/message.rb'
 
-get '/hi' do
-  "Hello World!"
+get '/' do
+  @messages = Message.order_by(:posted_date.desc)
+  haml :index
 end
 
+post '/add' do
+  Message.create({
+    :url => request[:url],
+    :posted_date => Time.now
+  })
+  redirect '/'
+end
---
+++ models/message.rb
@@ -0,0 +1,15 @@
+require 'sequel'
+Sequel::Model.plugin(:schema)
+
+Sequel.connect(ENV['DATABASE_URL'] || "sqlite://message.db")
+
+class Message < Sequel::Model
+  unless table_exists?
+    set_schema do
+      primary_key :id
+      text :url
+      timestamp :posted_date
+    end
+    create_table
+  end
+end
